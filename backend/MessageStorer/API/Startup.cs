da882fa6bbdca1e2c3573aa904e38389cda58a13
Marcin Jaworski <m.a.jaworski96@gmail.com>
2020-10-18 13:10:18 +0200

    Added swagger

--- backend/MessageStorer/API/Startup.cs
+++ backend/MessageStorer/API/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace API
 {
@@ -31,6 +34,37 @@ namespace API
 
             services.AddHttpContextAccessor();
 
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Message Storer API", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"Authorization token",
+                    // TODO: Authorization
+                    Name = "X-MockedAuthority",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                    }
+                });
+            });
             services.AddScoped<IApplicationRepository, ApplicationRepository>();
             services.AddScoped<IAppUserRepository, AppUserRepository>();
             services.AddScoped<IContactRepository, ContactRepository>();
@@ -66,6 +100,13 @@ namespace API
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Message Storer API v1");
+            });
+
         }
     }
 }
