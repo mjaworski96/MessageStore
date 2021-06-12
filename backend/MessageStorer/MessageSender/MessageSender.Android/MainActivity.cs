bc804581cef3666d78d1a467057cfd9e82034359
Marcin Jaworski <m.a.jaworski96@gmail.com>
2021-06-12 11:07:29 +0200

    [FRONTEND][ANDROID] Added icons

--- backend/MessageStorer/MessageSender/MessageSender.Android/MainActivity.cs
+++ backend/MessageStorer/MessageSender/MessageSender.Android/MainActivity.cs
@@ -1,13 +1,8 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
 using MessageSender.Droid.DeviceServices;
-using Android.Content;
 
 namespace MessageSender.Droid
 {
