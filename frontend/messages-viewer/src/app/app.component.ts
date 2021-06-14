d5ed1c801ac5d067bf9cf51a820ca2d76a1ae3aa
Marcin Jaworski <m.a.jaworski96@gmail.com>
2021-06-14 21:10:27 +0200

    [FRONTEND] Update app title and lang

--- frontend/messages-viewer/src/app/app.component.ts
+++ frontend/messages-viewer/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, Inject, LOCALE_ID, OnInit, ViewChild} from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'messages-viewer';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('title', {static: false})
+  translatedTitle: ElementRef;
+
+  constructor(private titleService: Title,
+    @Inject(DOCUMENT) private document: Document,
+    @Inject(LOCALE_ID) private locale: string) {}
+
+  ngAfterViewInit(): void {
+    this.titleService.setTitle(this.translatedTitle.nativeElement.innerHTML);
+    this.document.documentElement.lang = this.locale;
+  }
+
 }
